459bf3f7d01babb93e1c2f93e8a4f3563c6ce210
misha-pavlov <m.pavlov@apiko.com>
2023-10-24 16:11:52 -0500

    feat(ProfileImage): add launchImagePicker

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -1,8 +1,9 @@
-import { Avatar, Center } from "native-base";
+import { Avatar, Pressable } from "native-base";
 import { ThemeComponentSizeType } from "native-base/lib/typescript/components/types";
-import { FC } from "react";
+import { FC, useCallback } from "react";
 import { FontAwesome } from "@expo/vector-icons";
 import { colors } from "../config/colors";
+import { launchImagePicker } from "../utils/imagePickerHelper";
 
 type ProfileImagePropsTypes = {
   userInitials: string;
@@ -10,25 +11,29 @@ type ProfileImagePropsTypes = {
 };
 
 const ProfileImage: FC<ProfileImagePropsTypes> = ({ userInitials, size }) => {
+  const pickImage = useCallback(() => launchImagePicker(), []);
+
   return (
-    <Avatar
-      bg="cyan.500"
-      source={{
-        uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-      }}
-      size={size}
-    >
-      {userInitials}
-      <Avatar.Badge
-        bg={colors.lightGrey}
-        borderWidth={1}
-        borderColor={colors.lightGrey}
-        alignItems="center"
-        justifyContent="center"
+    <Pressable _pressed={{ opacity: 0.5 }} onPress={pickImage}>
+      <Avatar
+        bg="cyan.500"
+        source={{
+          uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        }}
+        size={size}
       >
-        <FontAwesome name="pencil" size={15} color="black" />
-      </Avatar.Badge>
-    </Avatar>
+        {userInitials}
+        <Avatar.Badge
+          bg={colors.lightGrey}
+          borderWidth={1}
+          borderColor={colors.lightGrey}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <FontAwesome name="pencil" size={15} color="black" />
+        </Avatar.Badge>
+      </Avatar>
+    </Pressable>
   );
 };
 
---
+++ utils/imagePickerHelper.ts
@@ -0,0 +1,30 @@
+import * as ImagePicker from "expo-image-picker";
+import { Platform } from "react-native";
+
+export const launchImagePicker = async () => {
+  await checkMediaPermissions();
+
+  const result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    aspect: [1, 1],
+    quality: 1,
+  });
+
+  if (!result.canceled) {
+    console.log(result.assets);
+  }
+};
+
+const checkMediaPermissions = async () => {
+  if (Platform.OS !== "web") {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      return Promise.reject("We need permission to access your photos.");
+    }
+  }
+
+  return Promise.resolve();
+};
