65a7e4c465b8737eb768215f1487aa48123b98d7
misha-pavlov <m.pavlov@apiko.com>
2023-10-24 17:11:25 -0500

    feat(ProfileImage): add loading state

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -2,9 +2,12 @@ import { Avatar, Pressable } from "native-base";
 import { ThemeComponentSizeType } from "native-base/lib/typescript/components/types";
 import { FC, useCallback, useState } from "react";
 import { FontAwesome } from "@expo/vector-icons";
+import { ActivityIndicator } from "react-native";
 import { colors } from "../config/colors";
 import { launchImagePicker, uploadImage } from "../utils/imagePickerHelper";
 import { updateUserData } from "../utils/actions/authActions";
+import { useAppDispatch } from "../store/store";
+import { updateUserDataRedux } from "../store/authSlice";
 
 type ProfileImagePropsTypes = {
   userId: string;
@@ -20,45 +23,60 @@ const ProfileImage: FC<ProfileImagePropsTypes> = ({
   uri,
 }) => {
   const [image, setImage] = useState(uri);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useAppDispatch();
   const pickImage = useCallback(async () => {
     try {
       const tempUri = await launchImagePicker();
 
       if (!tempUri) return;
 
+      setIsLoading(true);
       const uploadedUri = await uploadImage(tempUri);
+      setIsLoading(false);
 
       if (!uploadedUri) {
         throw new Error("Could not upload image");
       }
 
-      await updateUserData(userId, { profilePicture: uploadedUri });
+      const newData = { profilePicture: uploadedUri };
+      await updateUserData(userId, newData);
+      dispatch(updateUserDataRedux({ newData }));
       setImage(uploadedUri);
     } catch (error) {
       console.error(error);
+      setIsLoading(false);
     }
   }, [userId]);
 
   return (
     <Pressable _pressed={{ opacity: 0.5 }} onPress={pickImage}>
-      <Avatar
-        bg="cyan.500"
-        source={{
-          uri: image,
-        }}
-        size={size}
-      >
-        {userInitials}
-        <Avatar.Badge
+      {isLoading ? (
+        <ActivityIndicator
+          // 'as string' just for comparing
+          size={["md", "sm", "xs"].includes(size as string) ? "small" : "large"}
+          color={colors.primaryGreen}
+        />
+      ) : (
+        <Avatar
           bg={colors.lightGrey}
-          borderWidth={1}
-          borderColor={colors.lightGrey}
-          alignItems="center"
-          justifyContent="center"
+          source={{
+            uri: image,
+          }}
+          size={size}
         >
-          <FontAwesome name="pencil" size={15} color="black" />
-        </Avatar.Badge>
-      </Avatar>
+          {userInitials}
+          <Avatar.Badge
+            bg={colors.lightGrey}
+            borderWidth={1}
+            borderColor={colors.lightGrey}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <FontAwesome name="pencil" size={15} color="black" />
+          </Avatar.Badge>
+        </Avatar>
+      )}
     </Pressable>
   );
 };
