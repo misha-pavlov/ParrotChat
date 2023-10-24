cc1ec33d5c2b19484118573c8525413b16bd6447
misha-pavlov <m.pavlov@apiko.com>
2023-10-24 16:50:39 -0500

    feat(ProfileImage): add uploading image to firebase storage

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -1,24 +1,46 @@
 import { Avatar, Pressable } from "native-base";
 import { ThemeComponentSizeType } from "native-base/lib/typescript/components/types";
-import { FC, useCallback } from "react";
+import { FC, useCallback, useState } from "react";
 import { FontAwesome } from "@expo/vector-icons";
 import { colors } from "../config/colors";
-import { launchImagePicker } from "../utils/imagePickerHelper";
+import { launchImagePicker, uploadImage } from "../utils/imagePickerHelper";
 
 type ProfileImagePropsTypes = {
   userInitials: string;
   size: ThemeComponentSizeType<"Avatar">;
+  uri?: string;
 };
 
-const ProfileImage: FC<ProfileImagePropsTypes> = ({ userInitials, size }) => {
-  const pickImage = useCallback(() => launchImagePicker(), []);
+const ProfileImage: FC<ProfileImagePropsTypes> = ({
+  userInitials,
+  size,
+  uri,
+}) => {
+  const [image, setImage] = useState(uri);
+  const pickImage = useCallback(async () => {
+    try {
+      const tempUri = await launchImagePicker();
+
+      if (!tempUri) return;
+
+      const uploadedUri = await uploadImage(tempUri);
+
+      if (!uploadedUri) {
+        throw new Error("Could not upload image");
+      }
+
+      setImage(uploadedUri);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
 
   return (
     <Pressable _pressed={{ opacity: 0.5 }} onPress={pickImage}>
       <Avatar
         bg="cyan.500"
         source={{
-          uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+          uri: image,
         }}
         size={size}
       >
--- utils/imagePickerHelper.ts
+++ utils/imagePickerHelper.ts
@@ -1,20 +1,13 @@
 import * as ImagePicker from "expo-image-picker";
 import { Platform } from "react-native";
-
-export const launchImagePicker = async () => {
-  await checkMediaPermissions();
-
-  const result = await ImagePicker.launchImageLibraryAsync({
-    mediaTypes: ImagePicker.MediaTypeOptions.Images,
-    allowsEditing: true,
-    aspect: [1, 1],
-    quality: 1,
-  });
-
-  if (!result.canceled) {
-    console.log(result.assets);
-  }
-};
+import uuid from "react-native-uuid";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { getFirebaseApp } from "./firebaseHelper";
 
 const checkMediaPermissions = async () => {
   if (Platform.OS !== "web") {
@@ -27,4 +20,48 @@ const checkMediaPermissions = async () => {
   }
 
   return Promise.resolve();
+};
+
+export const launchImagePicker = async () => {
+  await checkMediaPermissions();
+
+  const result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    aspect: [1, 1],
+    quality: 1,
+  });
+
+  if (!result.canceled) {
+    return result.assets[0].uri;
+  }
+};
+
+export const uploadImage = async (uri: string) => {
+  const app = getFirebaseApp();
+  const blob: Blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+
+    xhr.onerror = function (e) {
+      console.error(e);
+      reject(new TypeError("Network request failed!"));
+    };
+
+    xhr.responseType = "blob";
+    xhr.open("GET", uri, true);
+    xhr.send();
+  });
+
+  const pathFolder = "profilePics";
+  const storageRef = ref(getStorage(app), `${pathFolder}/${uuid.v4()}`);
+
+  await uploadBytesResumable(storageRef, blob);
+
+  // @ts-ignore - because blob actually has close function
+  blob.close();
+
+  return getDownloadURL(storageRef);
 };
